060ead05790d798bcb516e90299dc05ceddd0219
Evgen Serdyuk <all.nothing.name@gmail.com>
2014-07-20 19:43:32 +0300

    add android project

---
+++ Pacman/proj.android/jni/Android.mk
@@ -0,0 +1,38 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := cocos2dcpp_shared
+
+LOCAL_MODULE_FILENAME := libcocos2dcpp
+
+LOCAL_SRC_FILES := hellocpp/main.cpp \
+                   ../../Classes/AppDelegate.cpp \
+				   ../../Classes/View/LevelMenuScene.cpp\
+				   ../../Classes/View/MainMenuScene.cpp\
+				   ../../Classes/View/WorldScene.cpp\
+				   ../../Classes/Model/Brick.cpp\
+				   ../../Classes/Model/Player.cpp\
+				   ../../Classes/Model/Point.cpp\
+				   ../../Classes/Model/Rectangle.cpp\
+				   ../../Classes/Model/World.cpp\
+				   ../../Classes/Model/WorldObject.cpp\
+				   ../../Classes/Model/WorldObjectMove.cpp\
+				   ../../Classes/Model/Spirit/Spirit.cpp\
+				   ../../Classes/Model/Spirit/Blinky.cpp\
+				   ../../Classes/Model/Spirit/Pinky.cpp\
+				   ../../Classes/Model/Spirit/Inky.cpp\
+				   ../../Classes/Model/Spirit/Clyde.cpp\
+				   
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../Classes
+
+LOCAL_WHOLE_STATIC_LIBRARIES := cocos2dx_static
+LOCAL_WHOLE_STATIC_LIBRARIES += cocosdenshion_static
+LOCAL_WHOLE_STATIC_LIBRARIES += box2d_static
+
+
+include $(BUILD_SHARED_LIBRARY)
+
+$(call import-module,2d)
+$(call import-module,audio/android)
+$(call import-module,Box2D)
