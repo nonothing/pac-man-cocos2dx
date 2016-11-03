98a7838a49374903db10f6bff3606a466994d4f9
name? <all.nothing.name@gmail.com>
2016-11-03 03:27:08 +0200

    fixed ios project

--- Pacman/Classes/AppDelegate.cpp
+++ Pacman/Classes/AppDelegate.cpp
@@ -7,6 +7,17 @@ USING_NS_CC;
 AppDelegate::AppDelegate() {}
 AppDelegate::~AppDelegate() {}
 
+//if you want a different context,just modify the value of glContextAttrs
+//it will takes effect on all platforms
+void AppDelegate::initGLContextAttrs()
+{
+    //set OpenGL context attributions,now can only set six attributions:
+    //red,green,blue,alpha,depth,stencil
+    GLContextAttrs glContextAttrs = {8, 8, 8, 8, 24, 8};
+    
+    GLView::setGLContextAttrs(glContextAttrs);
+}
+
 bool AppDelegate::applicationDidFinishLaunching() {
 
     auto director = Director::getInstance();
